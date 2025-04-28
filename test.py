d486031e2eeed518e53f00d68e57ed9bd3e5a8a8
Frank Wang <broodstar_frank@hotmail.com>
2025-04-28 08:38:23 -0400

    adjust test script to take action via probability dist rather than argmax

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from ray.rllib.algorithms import Algorithm
 import numpy as np
+import torch
 from environments import MazeEnv
 from maze import print_maze
 
@@ -17,7 +18,11 @@ def manualRun(mazeSize: int, agent: Algorithm, env: MazeEnv, envConfig):
         batched_obs = {"vision": np.expand_dims(obs["vision"], axis=0)}
         _, _, info = policy.compute_actions(batched_obs, full_fetch=True)
         policyLogits = info["action_dist_inputs"][0]
-        action = np.argmax(policyLogits)
+        action = np.random.choice(
+            [0, 1, 2, 3],
+            size=1,
+            p=torch.nn.functional.softmax(torch.Tensor(policyLogits), dim=0).numpy(),
+        )[0]
         obs, reward, done, _, info = env.step(action)  # Step the environment
         steps += 1
         maze[env._agentLocation[0]][env._agentLocation[1]] += 1

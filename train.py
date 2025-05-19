fix: evaluation reward mean with low precision

train.py
@@ -157,7 +157,7 @@ if __name__ == "__main__":
         if "evaluation" in result and i % args.evalInterval == 0:
             print(
                 f"Iteration {i + 1}:",
-                np.rint(result["evaluation"]["env_runners"]["episode_return_mean"]),
+                np.round(result["evaluation"]["env_runners"]["episode_return_mean"], 2),
                 " - ",
                 datetime.now(),
             )
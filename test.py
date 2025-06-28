b276cf05116a0ac6d3f6bbf44b707249d07b6fb4
Frank Wang <broodstar_frank@hotmail.com>
2025-06-28 09:42:12 -0400

    accommodate manual run for both fogged and place environments

--- test.py
+++ test.py
@@ -3,11 +3,13 @@ from ray.rllib.utils.numpy import convert_to_numpy, softmax
 from ray.rllib.core.rl_module.rl_module import RLModule
 import numpy as np
 import torch
-from environments import FoggedMazeEnv
+from environments import FoggedMazeEnv, PlaceMazeEnv
 from maze import print_maze
 
 
-def manualRun(mazeSize: int, module: RLModule, env: FoggedMazeEnv, envConfig):
+def manualRun(
+    mazeSize: int, module: RLModule, env: FoggedMazeEnv | PlaceMazeEnv, envConfig
+):
     env = env(envConfig)
     obs, _ = env.reset()
     goalLocation = env._goalLocation
@@ -29,16 +31,17 @@ def manualRun(mazeSize: int, module: RLModule, env: FoggedMazeEnv, envConfig):
     mazeTracker[mazeSize // 2][mazeSize // 2] = "S"
     mazeTracker[goalLocation[0]][goalLocation[1]] = "*"
 
-    previousState = torch.zeros([1, module.linearHiddenSize], dtype=torch.float32)
+    previousState = module.get_initial_state()
     while not done and steps < 1000:
         batched_obs = {
-            Columns.OBS: torch.from_numpy(np.array(env._memory)).unsqueeze(0),
-            Columns.STATE_IN: {"h": previousState},
+            Columns.OBS: torch.from_numpy(obs).unsqueeze(0).unsqueeze(0),
+            Columns.STATE_IN: {
+                k: torch.reshape(v, [1, -1]) for k, v in previousState.items()
+            },
         }
         rl_module_out = module.forward_inference(batched_obs)
-        logits = convert_to_numpy(rl_module_out[Columns.ACTION_DIST_INPUTS])[0, -1]
+        logits = convert_to_numpy(rl_module_out[Columns.ACTION_DIST_INPUTS])[0, 0]
         action = np.random.choice(env.action_space.n, p=softmax(logits))
-        previousMemoryLen = len(env._memory)
         obs, reward, done, truncated, info = env.step(action)
         done = done or truncated
         steps += 1
@@ -46,8 +49,7 @@ def manualRun(mazeSize: int, module: RLModule, env: FoggedMazeEnv, envConfig):
         if isinstance(agentLocationValue, int) and agentLocationValue < 9:
             mazeTracker[env._agentLocation[0]][env._agentLocation[1]] += 1
         totalReward += reward
-        if previousMemoryLen == 10:
-            previousState = rl_module_out[Columns.EMBEDDINGS][:, 0]
+        previousState = rl_module_out[Columns.STATE_OUT]
 
     for i in range(mazeSize):
         for j in range(mazeSize):

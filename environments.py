fix: remove redundant rotation scaling

environments.py
@@ -377,7 +377,7 @@ class ContinuousMazeEnv(FoggedMazeEnv):
         elif speed > 1:
             speed = 1
         speed /= 20
-        self._currentDirection = self._currentDirection + action[1] * np.pi
+        self._currentDirection = self._currentDirection + action[1]
         posChange = np.array(
             [
                 speed * np.cos(self._currentDirection),

models/agent_models.py
@@ -289,7 +289,7 @@ class VectorPredictor(nn.Module):
 
     def forward(self, logit):
         speed = self.speedPredictor(logit)
-        rotation = self.rotationPredictor(logit) * 3.14
+        rotation = self.rotationPredictor(logit)
         stglog = self.stdLogPredictor(logit)
         return torch.concat([speed, rotation, stglog], dim=2)
 
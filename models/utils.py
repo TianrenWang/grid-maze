db505ba435388c6e042e4de217a96e1eada3341d
Frank Wang <broodstar_frank@hotmail.com>
2026-07-09 08:28:15 -0400

    fix: field size not squared in place cell activation calculation

--- models/utils.py
+++ models/utils.py
@@ -13,7 +13,7 @@ def calculatePlace(
         agentLocation = agentLocation.flatten(0, -2)
         diff = agentLocation.unsqueeze(1) - placeCells.unsqueeze(0)
         dists_squared = torch.sum(torch.abs(diff) ** 2, dim=-1)
-        unnormalized_activations = -dists_squared / (2 * fieldSize)
+        unnormalized_activations = -dists_squared / (2 * fieldSize**2)
         normalized_activations = torch.nn.functional.softmax(
             unnormalized_activations, dim=1
         )

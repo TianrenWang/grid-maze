cdd5443f8b867246086b9fc16aca2bd75f8e4b3f
Frank Wang <broodstar_frank@hotmail.com>
2026-07-11 11:48:35 -0400

    fix: self localization has access to the expanded environment

--- train.py
+++ train.py
@@ -54,6 +54,15 @@ if __name__ == "__main__":
         maze = generateMazeWithOfflimit(mazeSize)
     elif args.selfLocalize:
         maze = generateMaze(mazeSize * 2, 0)
+        for i in range(len(maze)):
+            for j in range(len(maze[0])):
+                if (
+                    mazeSize // 2 + mazeSize < i
+                    or i < mazeSize // 2
+                    or mazeSize // 2 + mazeSize < j
+                    or j < mazeSize // 2
+                ):
+                    maze[i][j] = 0
     elif not args.randomMaze:
         if not os.path.exists(mazesPath):
             os.makedirs(mazesPath)

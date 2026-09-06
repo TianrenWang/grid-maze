645aa70a869a20173a2409c3c61a2924a47ec5e6
Frank Wang <broodstar_frank@hotmail.com>
2026-09-06 17:07:58 -0400

    further standardize default training parameters

--- train_latent_path.sh
+++ train_latent_path.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python train.py "$1" "$2" --pretrain --numLearn 600 --maxSteps 58
-cp -r checkpoints/"$2" checkpoints/jepa-pretrained
-python train.py "$1" "$2" --learnManifold --maxSteps 58
+python train.py --expName "$1" --pretrain --numLearn 300
+cp -r checkpoints/"$1" checkpoints/"$1"-pretrained
+python train.py --expName "$1" --learnManifold

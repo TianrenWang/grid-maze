cd13bf6995820aa902a2c308e4cc1826704378b3
Frank Wang <broodstar_frank@hotmail.com>
2026-09-06 17:45:13 -0400

    shorten pretraining time

--- train_latent_path.sh
+++ train_latent_path.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python train.py --expName "$1" --pretrain --numLearn 300
+python train.py --expName "$1" --pretrain --numLearn 200
 cp -r checkpoints/"$1" checkpoints/"$1"-pretrained
 python train.py --expName "$1" --learnManifold
